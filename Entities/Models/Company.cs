6b06d610d48274e439d8e669c827f8a7b02b1b2d
peax3 <peaceadjerese@gmail.com>
2021-06-28 21:38:25 +0100

    Add: company model and employee model

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+   public class Company
+   {
+      [Key]
+      public Guid CompanyId { get; set; }
+
+      [Required(ErrorMessage = "Company name is required")]
+      [MaxLength(40, ErrorMessage = "Maximum length for the Company name is 40 characters.")]
+      public string Name { get; set; }
+
+      [Required(ErrorMessage = "Company address is required")]
+      public string Address { get; set; }
+
+      // relationship
+      public ICollection<Employee> Employees { get; set; }
+   }
+}
---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+   public class Employee
+   {
+      [Key]
+      public Guid EmployeeId { get; set; }
+
+      [Required(ErrorMessage = "Employee address is required")]
+      [MaxLength(20, ErrorMessage = "Maximum length for Employee name is 20 characters.")]
+      public string Name { get; set; }
+
+      [Required(ErrorMessage = "Employee age is a required")]
+      public int Age { get; set; }
+
+      [Required(ErrorMessage = "Position is a required field.")]
+      [MaxLength(20, ErrorMessage = "Maximum length for Employee position is 20 characters.")]
+      public string Position { get; set; }
+
+      // relationship
+      [ForeignKey(nameof(Company))]
+      public Guid CompanyId { get; set; }
+      public Company Company { get; set; }
+
+   }
+}
