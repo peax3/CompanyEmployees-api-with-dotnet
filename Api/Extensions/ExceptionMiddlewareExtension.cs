ad21dce56ee91e4fef61a93c326d393cb154253a
peax3 <peaceadjerese@gmail.com>
2021-06-30 09:50:19 +0100

    Add: Error middleware handler

---
+++ Api/Extensions/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Api.Extensions
+{
+   public static class ExceptionMiddlewareExtension
+   {
+      public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+      {
+         app.UseExceptionHandler(appError =>
+         {
+            appError.Run(async context =>
+            {
+               context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+               context.Response.ContentType = "application/json";
+
+               var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+               if (contextFeature != null)
+               {
+                  await context.Response.WriteAsync(
+                     JsonSerializer.Serialize(
+                        new
+                        {
+                           StatusCode = context.Response.StatusCode,
+                           Message = "Internal server error"
+                        })
+                  );
+               }
+            });
+         });
+      }
+   }
+}
